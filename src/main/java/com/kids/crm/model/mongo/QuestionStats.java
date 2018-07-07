b64322a3dcb0777ad6e7fb678d5c47ec1693a004
Tamjid Sarker <oleeve031@gmail.com>
2018-07-07 18:04:39 +0600

    Added Builder and Document annotaion

--- src/main/java/com/kids/crm/model/mongo/QuestionStats.java
+++ src/main/java/com/kids/crm/model/mongo/QuestionStats.java
@@ -1,13 +1,17 @@
 package com.kids.crm.model.mongo;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 import java.util.List;
 
 @Getter
 @Setter
+@Builder
+@Document
 public class QuestionStats {
     private String questionId;
     private String skipCount;
